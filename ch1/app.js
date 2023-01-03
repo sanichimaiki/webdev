315ae24a3cb2bb5116899a7e162d6c41af44443b
sanichimaiki <michaeldeemoreland@gmail.com>
2023-01-02 18:41:15 -0800

    ファイルを整えたり作った。

---
+++ ch1/app.js
@@ -0,0 +1,13 @@
+/* This JS will:
+    - set a variable "a" to 10
+    - output the value of "a" to the console
+    - create a popup that will say a message
+    - output my name into the console
+
+*/
+
+let a = 10; // This will set variable "a" to 10
+console.log(a); // This will print the value of "a" to the console
+
+alert("Saying hi from a different file!");
+console.log("it's maiki!");
