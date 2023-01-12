220a372e2affc872311ff982097c863892e61378
sanichimaiki <michaeldeemoreland@gmail.com>
2023-01-11 22:10:19 -0800

    続け

--- ch5/loops.js
+++ ch5/loops.js
@@ -17,3 +17,22 @@ for(let i = 0; i < arrKeys.length; i++) {
 //     console.log(`${key}: ${car[key]}`);
 // }
 
+let car2 = {
+    model: "Cute",
+    make: "Prius",
+    color: "blue",
+    year: 1998
+}
+
+let cars = [car, car2];
+
+// continue
+for(let car of cars) {
+    // If the car color IS NOT black, break out of the current iteration, move back up to check the condition, and start a new iteration
+    if(car.color !== 'black') {
+        continue;
+    }
+    if(car.year <= 2005) {
+        console.log(`we found this car ${car}`)
+    }
+}
