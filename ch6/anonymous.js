d372377604e01f9da1cd781a4a9f443e3e6f5030
sanichimaiki <michaeldeemoreland@gmail.com>
2023-01-21 22:02:27 -0800

    無名の関数

---
+++ ch6/anonymous.js
@@ -0,0 +1,19 @@
+let anonfunc = function(para1) {
+    console.log(para1);
+};
+
+anonfunc("hiiii");
+
+function normalfunc(para1) {
+    console.log(para1);
+}
+
+normalfunc("hey wyd");
+
+let anonfunc2 = (para1) => {
+    console.log(para1);
+};
+
+anonfunc2("you free tn");
+
+// All of three of these functions should do the same thing
