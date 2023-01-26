b708acfe0734f16eb31275a4dfe14ce73c8c07e9
sanichimaiki <michaeldeemoreland@gmail.com>
2023-01-25 21:40:00 -0800

    ゲッターとセッター

--- ch7/classes.js
+++ ch7/classes.js
@@ -5,3 +5,36 @@ class Animal {
         this.color = color;
     }
 }
+
+class Person {
+    // # indicates privacy; the object is in control of its own fields
+    #firstname;
+    #lastname;
+    constructor(firstname, lastname) {
+        this.#firstname = firstname;
+        this.#lastname = lastname;
+    }
+
+    get firstname() {
+        return this.#firstname;
+    }
+    get lastname() {
+        return this.#lastname;
+    }
+
+    set firstname(firstname) {
+        this.#firstname = firstname;
+    }
+    set lastname(lastname) {
+        this.#lastname = lastname;
+    }
+
+    fullname() {
+        return `${this.firstname} ${this.lastname}`;
+    }
+}
+
+let arxn = new Person("arxn", "ethereal");
+
+console.log(`<3 ${arxn.firstname}`);
+console.log(arxn.fullname());
